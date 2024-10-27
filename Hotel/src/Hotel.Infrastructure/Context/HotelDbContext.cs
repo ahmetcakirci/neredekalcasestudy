f712f90ba16354c26c9326e9cfd0bd363b7e2da9
ahmetcakirci <ahmetcakirci@gmail.com>
2024-10-27 22:33:00 +0300

    feat(Hotel):Add Create repository

--- Hotel/src/Hotel.Infrastructure/Context/HotelDbContext.cs
+++ Hotel/src/Hotel.Infrastructure/Context/HotelDbContext.cs
@@ -6,10 +6,6 @@ namespace Infrastructure.Context;
 public class HotelDbContext: DbContext
 {
     public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
-
-    public DbSet<Hotel> Hotels { get; set; }
-    public DbSet<ContactInfo> ContactInfos { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyRefence).Assembly);
