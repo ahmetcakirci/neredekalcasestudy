38a877007c559eb355d13e930e8cf12a17d6130c
ahmetcakirci <ahmetcakirci@gmail.com>
2024-10-29 20:09:26 +0300

    feat(Report):Add Worker service Refit client

---
+++ Raport/src/Report.Infrastructure/Clients/IHotelReport.cs
@@ -0,0 +1,11 @@
+using Refit;
+using Report.Domain.DTOs;
+
+namespace Report.Infrastructure.Clients;
+
+public interface IHotelReport
+{
+    [Headers("Content-Type: application/json")]
+    [Get("/api/Hotel/GetLocationReport")]
+    Task<ApiResponse<ReportDto>> GetHotelLocation();
+}
